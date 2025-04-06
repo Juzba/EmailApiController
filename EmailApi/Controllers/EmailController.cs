b2f963daade5d3771fe8dc7807818fb657688688
Jiří Strnadel <Juzba88@gmail.com>
2025-04-06 13:34:29 +0200

    Přidejte soubory projektu.

---
+++ EmailApi/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailApi.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+
+
+        [HttpPost("send")]
+
+        public IActionResult SendEmail([FromBody] EmailRequest emailRequest)
+        {
+            return Ok("E-mail byl úspěšně odeslán.");
+        }
+    }
+}
+
+public class EmailRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+}
